6a49a9c90c48568379798afb7ed7e2e1141006eb
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-20 10:38:00 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ title, onClick, isValid }: IButtonProps) => {
+  return <ButtonContainer 
+  onClick={onClick}
+  disabled={!isValid}
+  >
+    {title}
+  </ButtonContainer>;
+};
+
+export default Button;
